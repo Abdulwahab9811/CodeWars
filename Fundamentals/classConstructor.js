007662014f6a37422ce3067eb50e0bee31fd24d6
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-03-26 21:37:26 -0400

    Class Methods

--- Fundamentals/classConstructor.js
+++ Fundamentals/classConstructor.js
@@ -3,8 +3,28 @@ class Surgeon {
     constructor(name, department) {
       this.name = name;
       this.department = department;
+      this._remainingVacationDays = 20;
+    }
+
+    get name() {
+      return this._name;
+    }
+    
+    get department() {
+      return this._department;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+//     create a method called takeVacationDays that accepts one argument named daysOff.
+// Inside of the method, subtract daysOff from the number saved to _remainingVacationDays. Set _remainingVacationDays to the result.
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
     }
   }
+  
   
   //create class instances. An instance is an object that contains the property names and methods of a class, but with unique property values. 
   const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');

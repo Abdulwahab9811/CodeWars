4dff6f05fd83395d2c907582e415d649f1cfd48a
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-03-28 14:11:05 -0400

    Parentclasses

--- Fundamentals/classConstructor.js
+++ Fundamentals/classConstructor.js
@@ -29,3 +29,31 @@ class Surgeon {
   //create class instances. An instance is an object that contains the property names and methods of a class, but with unique property values. 
   const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
   const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+//Calculating is remaining vacations
+  console.log(surgeonRomero.name)
+  surgeonRomero.takeVacationDays(3)
+
+  console.log(surgeonRomero.remainingVacationDays)
+
+
+  '*****************************************************'
+
+
+  //Parent Classe that takes in 2 "SubClasses"
+
+  class HospitalEmployee {
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+  
+    get name(){
+      return this._name;
+    }
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff
+    }
+  }

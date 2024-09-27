c86706be5f13a991a06807fe9b3820a58e28cc70
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-09-27 17:40:37 -0400

    classconstructor parent/subclasse

--- Fundamentals/classConstructor.js
+++ Fundamentals/classConstructor.js
@@ -83,3 +83,102 @@ console.log(nurseOlynyk.remainingVacationDays);
 nurseOlynyk.addCertification('Genetics')
 
 console.log(nurseOlynyk.certifications);;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CLASS CONSTRUCTOR STATIC METHODE FOR randomBorough() for apartement or rentalunit
+
+class RentalUnit {
+  constructor(address, costPerYear) {
+    this._address = address;
+    this._costPerYear = costPerYear;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get costPerYear() {
+    return this._costPerYear;
+  }
+
+  calculateMonthly() {
+    return this.costPerYear / 12; 
+  }
+}
+
+class Apartment extends RentalUnit {
+  constructor(address, costPerYear, numberOfBedrooms) {
+    super(address, costPerYear);
+    this._numberOfBedrooms = numberOfBedrooms;
+  }
+
+  get numberOfBedrooms() {
+    return this._numberOfBedrooms;
+  }
+
+  static randomBorough() {
+    const myBoroughs = ['Manhattan', 'Brooklyn', 'Queens', 'The Bronx', 'Staten Island'];
+    return myBoroughs[Math.floor(Math.random() * 5)];
+  }
+}
+
+const myApartment = new Apartment('1234 W 54th', 22000, 3);
+
+
+console.log(Apartment.randomBorough());
+
+console.log(RentalUnit.randomBorough());
+
+////////////////////////////////////////////////////////
+
+
+
+// Parent class (Super class)
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a sound.`);
+  }
+}
+
+// Child class (Subclass) inherits from Animal
+class Dog extends Animal {
+  constructor(name, breed) {
+    // super() calls the parent class's constructor
+    super(name);  // Calls Animal's constructor to set the 'name' property
+    this.breed = breed; // Additional property specific to Dog
+  }
+
+  // Overriding the speak method from the parent class
+  speak() {
+    // super.speak() calls the parent class's speak method
+    super.speak();  // Calls the Animal's speak method
+    console.log(`${this.name} is a ${this.breed} and barks.`);
+  }
+}
+
+// Create an instance of the Dog class
+const myDog = new Dog("Buddy", "Golden Retriever");
+myDog.speak();
+// Outputs:
+// Buddy makes a sound.
+// Buddy is a Golden Retriever and barks.
